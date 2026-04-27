1a708ec889a9810f606d4f16547123f536dcb3dc
SBHattarj <subhambhsubhambh@gmail.com>
2026-04-27 16:41:10 +0530

    removed unnecesarry files.

--- bin/python/mouse_move_up.py
+++
@@ -1,7 +0,0 @@
-import uinput
-import time
-import sys
-print(sys.argv)
-with uinput.Device([uinput.REL_X, uinput.REL_Y, uinput.BTN_LEFT, uinput.BTN_RIGHT]) as device:
-    time.sleep(0.05)
-    device.emit(uinput.REL_Y, -20)
